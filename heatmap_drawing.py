2c8b5c4743cd6d138dbcc89a2c574583d78a783c
ReturnOfTheLast <returnofthelast@gmail.com>
2022-11-14 17:37:12 +0100

    Changed heatmap data info to be more readable

--- heatmap_drawing.py
+++ heatmap_drawing.py
@@ -1,6 +1,22 @@
 from PIL import Image, ImageDraw
 from math import sqrt
 
+"""
+ap = {
+    "coords": (0, 0),
+    "label": "Access Point"
+}
+
+scans = [
+    {
+        "coords": (0, 0),
+        "rssi": -45,
+        "label": "scanning point 1"
+    },
+    ...
+]
+"""
+
 color_gradient = [
                     [0, 0, 255, 0],
                     [50, 255, 255, 0],
@@ -29,11 +45,16 @@ def getcolor(color_gradient: list, percent: int) -> tuple:
 def make_image(width: int, height: int) -> Image.Image:
     return Image.new("RGB", (width, height), color=(0, 0, 0))
 
-def draw_heat_circles(im: Image.Image, ap: tuple, scans: list) -> None:
+def draw_heat_circles(im: Image.Image, ap: dict, scans: list[dict]) -> None:
     scan_dists = []
     for scan in scans:
-        dist = int(sqrt((ap[0] - scan[0])**2 + (ap[1] - scan[1])**2))
-        scan_dists.append((dist, int((-scan[2]/90) * 100)))
+        dist = int(
+            sqrt(
+                (ap["coords"][0] - scan["coords"][0])**2 +
+                (ap["coords"][1] - scan["coords"][1])**2
+            )
+        )
+        scan_dists.append((dist, int((-scan["rssi"]/90) * 100)))
 
     scan_dists.sort(key=lambda x: x[0], reverse=True)
 
@@ -41,15 +62,43 @@ def draw_heat_circles(im: Image.Image, ap: tuple, scans: list) -> None:
 
     for dist in scan_dists:
         color = getcolor(color_gradient, dist[1])
-        draw.ellipse([(ap[0] - dist[0], ap[1] - dist[0]), (ap[0] + dist[0], ap[1] + dist[0])], fill=color)
+        draw.ellipse(
+            [
+                (ap["coords"][0] - dist[0], ap["coords"][1] - dist[0]),
+                (ap["coords"][0] + dist[0], ap["coords"][1] + dist[0])
+            ],
+            fill=color
+        )
 
-def draw_scanning_points(im: Image.Image, scans: list) -> None:
+def draw_scanning_points(im: Image.Image, scans: list[dict]) -> None:
     draw = ImageDraw.Draw(im)
     for scan in scans:
-        draw.ellipse([(scan[0] - 5, scan[1] - 5), (scan[0] + 5, scan[1] + 5)], fill=(127, 127, 127))
-        draw.text((scan[0] + 10, scan[1]), scan[3], fill=(0, 255, 255), anchor="ls")
+        draw.ellipse(
+            [
+                (scan["coords"][0] - 5, scan["coords"][1] - 5),
+                (scan["coords"][0] + 5, scan["coords"][1] + 5)
+            ],
+            fill=(127, 127, 127)
+        )
+        draw.text(
+            (scan["coords"][0] + 10, scan["coords"][1]),
+            scan["label"],
+            fill=(0, 255, 255),
+            anchor="ls"
+        )
 
-def draw_accesspoint(im: Image.Image, ap: tuple) -> None:
+def draw_accesspoint(im: Image.Image, ap: dict) -> None:
     draw = ImageDraw.Draw(im)
-    draw.ellipse([(ap[0] - 5, ap[1] - 5), (ap[0] + 5, ap[1] + 5)], fill=(50, 50, 50))
-    draw.text((ap[0] + 10, ap[1]), ap[2], fill=(50, 50, 50), anchor="ls")
+    draw.ellipse(
+        [
+            (ap["coords"][0] - 5, ap["coords"][1] - 5),
+            (ap["coords"][0] + 5, ap["coords"][1] + 5)
+        ],
+        fill=(50, 50, 50)
+    )
+    draw.text(
+        (ap["coords"][0] + 10, ap["coords"][1]),
+        ap["label"],
+        fill=(50, 50, 50),
+        anchor="ls"
+    )
